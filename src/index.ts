7c47aeaaf8b8442843859c7665ac933d9bc4bf70
Antti Peuhkurinen <peuhkura@gmail.com>
2024-07-01 23:10:49 +0300

    adding access to local app

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,26 @@ app.use(express.json());
 
 const PORT = 3000;
 
+// Add headers before the routes are defined
+app.use(function (_req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
